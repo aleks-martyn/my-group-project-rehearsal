cc5778c530896857291a920793ec6131ef3ff2d1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-13 00:41:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+
+const galleryEl = document.querySelector('.gallery');
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
+const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
+const IMAGE_SIZE = 'w200';
 
 // class MoviesApiServise {
 //    constructor() {
@@ -22,17 +28,34 @@ function fetchPopularMovies() {
   });
 }
 //}
-fetchPopularMovies();
+fetchPopularMovies().then(data => {
+  const { page, results, total_pages, total_results } = data;
+  renderGallery(results);
+});
+
+function renderGallery(results) {
+  const markup = results
+    .map(result => {
+      return `<div class="movie-card">
+      <img class="movie-poster" src="${BASE_IMAGE_URL}${IMAGE_SIZE}${result.poster_path}" alt="${result.title}" loading="lazy" />
+      <div class="movie-info">
+      <p class="movie-title">
+      ${result.original_title}</p>
+      </div>`;
+    })
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
 
 function fetchMovies(searchQuery) {
-    const searchParams = new URLSearchParams({
-        api_key: `${API_KEY}`,
-        query: `${searchQuery}`,
-    });
-    
-    const url = `${BASE_URL}search/movie?${searchParams}`;
+  const searchParams = new URLSearchParams({
+    api_key: `${API_KEY}`,
+    query: `${searchQuery}`,
+  });
 
-    return fetch(url).then(response => {
+  const url = `${BASE_URL}search/movie?${searchParams}`;
+
+  return fetch(url).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -41,3 +64,13 @@ function fetchMovies(searchQuery) {
 }
 
 fetchMovies('avatar');
+
+const container = document.getElementById('tui-pagination-container');
+const instance = new Pagination(container, {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  centerAlign: true,
+});
+const pageNumber = instance.getCurrentPage();
+console.log(pageNumber);
