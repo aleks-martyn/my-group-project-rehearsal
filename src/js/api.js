d935d594aed0eed4a3ca28a015f4b97944996c1e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-20 00:54:04 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
-export { fetchPopularMovies, fetchGenresList, fetchMovieByKeyword };
+export {
+  fetchPopularMovies,
+  fetchGenresList,
+  fetchMovieByKeyword,
+  fetchMovieById,
+  fetchTrailerById,
+};
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
@@ -43,3 +49,44 @@ async function fetchMovieByKeyword(searchQuery) {
 
   return data;
 }
+
+async function fetchMovieById(movieId) {
+  const searchParams = new URLSearchParams({
+    api_key: `${API_KEY}`,
+  });
+
+  const url = `${BASE_URL}movie/${movieId}?${searchParams}`;
+  const dataObj = await axios.get(url);
+  const { data } = dataObj;
+
+  return data;
+}
+
+async function fetchTrailerById(movie_id) {
+  const searchParams = new URLSearchParams({
+    api_key: `${API_KEY}`,
+  });
+
+  const url = `${BASE_URL}movie/${movie_id}/videos?${searchParams}`;
+  const dataObj = await axios.get(url);
+  const { data } = dataObj;
+
+  return data.results;
+}
+
+async function fetchTrendingMovies() {
+  const searchParams = new URLSearchParams({
+    api_key: `${API_KEY}`,
+  });
+
+  const url = `${BASE_URL}trending/movie/week?${searchParams}`;
+  const dataObj = await axios.get(url);
+  const { data } = dataObj;
+
+  return data;
+}
+
+fetchTrendingMovies().then(data => {
+  console.log(data);
+  const { page, results, total_pages, total_results } = data;
+});
