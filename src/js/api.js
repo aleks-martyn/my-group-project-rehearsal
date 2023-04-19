148f2f0b94adeed69371015e501f85abf4d80477
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-19 22:27:17 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,52 +1,45 @@
 import axios from 'axios';
 export { fetchPopularMovies, fetchGenres, fetchMovies };
-  
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560'; 
 
-async function fetchPopularMovies(
-  api_key,
-  base_url,
-  pageNumber
-) {
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
+
+async function fetchPopularMovies(pageNumber) {
   const searchParams = new URLSearchParams({
-    api_key: `${api_key}`,
+    api_key: `${API_KEY}`,
     page: `${pageNumber}`,
     language: 'en-US',
   });
 
-  const url = `${base_url}/movie/popular?${searchParams}`;
+  const url = `${BASE_URL}/movie/popular?${searchParams}`;
   const dataObj = await axios.get(url);
   const { data } = dataObj;
 
   return data;
 }
 
-async function fetchGenres(api_key, base_url) {
+async function fetchGenres() {
   const searchParams = new URLSearchParams({
-    api_key: `${api_key}`,
+    api_key: `${API_KEY}`,
     language: 'en-US',
   });
 
-  const url = `${base_url}genre/movie/list?${searchParams}`;
+  const url = `${BASE_URL}genre/movie/list?${searchParams}`;
   const dataObj = await axios.get(url);
   const { data } = dataObj;
 
   return data.genres;
 }
 
-function fetchMovies(searchQuery) {
+async function fetchMovies(searchQuery) {
   const searchParams = new URLSearchParams({
     api_key: `${API_KEY}`,
     query: `${searchQuery}`,
   });
 
   const url = `${BASE_URL}search/movie?${searchParams}`;
+  const dataObj = await axios.get(url);
+  const { data } = dataObj;
 
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return data;
 }
