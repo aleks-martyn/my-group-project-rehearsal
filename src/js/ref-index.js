3a193022e7778d9ed3fd049d60f9f07447192ded
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-11 19:11:19 +0300

    Create ref-index.js

---
+++ src/js/ref-index.js
@@ -0,0 +1,7 @@
+const searchFormEl = document.querySelector('.nav__form');
+const inputEl = document.querySelector('.nav__input');
+const errorEl = document.querySelector('.header__error');
+const moviesEl = document.querySelector('.films');
+const container = document.getElementById('tui-pagination-container');
+
+export { searchFormEl, inputEl, errorEl, moviesEl, container };
