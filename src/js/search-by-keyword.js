e5950df22fd37a9a0823a0b3d48814b77ad13c7a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-12 00:16:57 +0300

    Update search-by-keyword.js

--- src/js/search-by-keyword.js
+++ src/js/search-by-keyword.js
@@ -10,3 +10,21 @@ import {
 } from './ref-index';
 import renderGallery from './render-gallery';
 import setScrollToUp from './set-scroll';
+
+searchFormEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const query = inputEl.value;
+  let page = 1;
+
+  fetchMovieByKeyword(query, page)
+    .then(data => {
+      const { page, results, total_pages, total_results } = data;
+      return renderGallery(results);
+    })
+    .then(async res => {
+      return (moviesEl.innerHTML = res);
+    })
+    .catch(console.log);
+}
