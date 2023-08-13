5cd4d3c105c04dc63aaefb41888c54de0c553bab
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-13 12:56:27 +0300

    Update search-by-keyword.js

--- src/js/search-by-keyword.js
+++ src/js/search-by-keyword.js
@@ -1,6 +1,6 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
-import { fetchMovieByKeyword } from './api';
+import getMovieByKeyword from './get-movie-by-keyword';
 import {
   searchFormEl,
   inputEl,
@@ -8,23 +8,14 @@ import {
   moviesEl,
   container,
 } from './ref-index';
-import renderGallery from './render-gallery';
 import setScrollToUp from './set-scroll';
 
 searchFormEl.addEventListener('submit', handleFormSubmit);
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
   const query = inputEl.value;
   let page = 1;
 
-  fetchMovieByKeyword(query, page)
-    .then(data => {
-      const { page, results, total_pages, total_results } = data;
-      return renderGallery(results);
-    })
-    .then(async res => {
-      return (moviesEl.innerHTML = res);
-    })
-    .catch(console.log);
+  await getMovieByKeyword(query, page);
 }
