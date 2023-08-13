a55526b5af827e1466a5122d1119c3248e98ac09
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-13 19:15:16 +0300

    Update get-movie-by-keyword.js

--- src/js/get-movie-by-keyword.js
+++ src/js/get-movie-by-keyword.js
@@ -1,11 +1,32 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+import { container } from './ref-index';
 import { fetchMovieByKeyword } from './api';
 import renderGallery from './render-gallery';
 import { moviesEl } from './ref-index';
+import setScrollToUp from './set-scroll';
 
 export default async function getMovieByKeyword(searchQuery, pageNumber) {
   try {
-    const { page, results, total_pages, total_results } =
-      await fetchMovieByKeyword(searchQuery, pageNumber);
+    const { results, total_results } = await fetchMovieByKeyword(
+      searchQuery,
+      pageNumber
+    );
+
+    const pagination = new Pagination(container, {
+      totalItems: total_results,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      centerAlign: true,
+    });
+
+    pagination.on('afterMove', async event => {
+      setScrollToUp();
+      const currentPage = event.page;
+      const { results } = await fetchMovieByKeyword(searchQuery, currentPage);
+      const res = await renderGallery(results);
+      return (moviesEl.innerHTML = res);
+    });
 
     const res = await renderGallery(results);
     return (moviesEl.innerHTML = res);
