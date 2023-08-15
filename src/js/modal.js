89473d007fd84c8b48a01b4aa445d94ec45c1102
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-15 22:28:19 +0300

    Create modal.js

---
+++ src/js/modal.js
@@ -0,0 +1,4 @@
+import * as basicLightbox from 'basiclightbox';
+import { fetchMovieById } from './api';
+import modalTpl from './templates/template-modal.hbs';
+import { moviesEl } from './ref-index';
