f011fe60bb98eb761bfffab57eab7c1143f8942e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-16 19:41:12 +0300

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = '404ca53f902a08bf3140e0fd0ad0a560';
+const API_KEY = process.env.NODE_ENV_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 const NO_POSTER = `https://i.ibb.co/r76r6Vt/oie-30214851-Ms-Wl-PTS0.png`;
