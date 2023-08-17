0fe36691500d1799539f9b35fc3dc97e59c8bae0
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-18 00:23:27 +0300

    Update ref-index.js

--- src/js/ref-index.js
+++ src/js/ref-index.js
@@ -3,5 +3,15 @@ const inputEl = document.querySelector('.nav__input');
 const errorEl = document.querySelector('.header__error');
 const moviesEl = document.querySelector('.movies');
 const container = document.getElementById('tui-pagination-container');
+const bodyEl = document.querySelector('body');
+const basicLightboxEl = document.querySelector('.basicLightbox');
 
-export { searchFormEl, inputEl, errorEl, moviesEl, container };
+export {
+  searchFormEl,
+  inputEl,
+  errorEl,
+  moviesEl,
+  container,
+  bodyEl,
+  basicLightboxEl,
+};
