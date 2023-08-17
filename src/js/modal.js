d5d0514137ecd81d7d57b959ac2f41071dff8347
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-17 14:53:06 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,3 +2,9 @@ import * as basicLightbox from 'basiclightbox';
 import { fetchMovieById } from './api';
 import modalTpl from './templates/template-modal.hbs';
 import { moviesEl } from './ref-index';
+
+moviesEl.addEventListener('click', handleMovieClick);
+
+async function handleMovieClick(event) {
+  console.log(event.target)
+}
