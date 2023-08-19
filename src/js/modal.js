eb49bc4977dfc9c71b780418c68cc6938b77c709
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-20 00:01:44 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 import * as basicLightbox from 'basiclightbox';
 import { moviesEl, bodyEl, basicLightboxEl } from './ref-index';
 import getMovieById from './get-movie-by-id';
+import { fetchTrailerById } from './api';
 
 moviesEl.addEventListener('click', handleMovieClick);
 
@@ -14,7 +15,30 @@ async function handleMovieClick(event) {
   const instance = basicLightbox.create(movie);
 
   instance.show(() => {
-    const closeBtnEl = document.querySelector('modal__close');
-    //bodyEl.classList.add('no-scroll');
+    const closeBtnEl = document.querySelector('.modal__close');
+    const playBtnEl = document.querySelector('.modal__play');
+
+    playBtnEl.addEventListener('click', handlePlayBtnClick);
+    bodyEl.classList.add('no-scroll');
+
+    async function handlePlayBtnClick(event) {
+      const movieId = event.target.dataset.mvid;
+
+      if (!movieId) return;
+      getTrailer(movieId);
+    }
   });
+}
+
+async function getTrailer(movieId) {
+  try {
+    const videos = await fetchTrailerById(movieId);
+    
+    if (!videos?.length) {
+      console.log("Sorry, we don't found any trailer");
+    }
+    console.log(videos);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
