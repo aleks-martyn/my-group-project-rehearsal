fdd822fea776e22129e632d0c157be0b65519062
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-20 17:55:47 +0300

    Create get-trailer.js

---
+++ src/js/get-trailer.js
@@ -0,0 +1,23 @@
+import { fetchTrailerById } from './api';
+import { BASE_TRAILER_URL } from './constants';
+import modalTrailerTpl from './templates/template-modal-trailer.hbs';
+import getTrailerKey from './get-trailer-key';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+export default async function getTrailer(movieId) {
+  try {
+    const videos = await fetchTrailerById(movieId);
+
+    if (!videos?.length) {
+      Report.failure("Sorry, we don't found any trailer");
+    }
+    console.log(videos);
+
+    const trailerKey = getTrailerKey(videos);
+    const trailerPath = `${BASE_TRAILER_URL}${trailerKey}`;
+
+    return modalTrailerTpl({ trailerPath });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
