98431c8a8772a807cb130bc9d89cc757d924c1b3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-02-17 14:46:46 +0200

    Update local-storage-functions.js

--- src/js/local-storage-functions.js
+++ src/js/local-storage-functions.js
@@ -16,10 +16,56 @@ export const save = (key, value) => {
   }
 };
 
-export const remove = key => window.localStorage.removeItem(key);
+export const checkMovieIsStored = (movies, id) =>
+  movies.map(({ id }) => id).includes(id);
 
-export const checkMovieIsStored = (key, id) => {
+export const saveMovie = (el, movies, key) => {
+  const {
+    mvid,
+    releaseDate,
+    genreIds,
+    titleString,
+    originalTitleString,
+    posterPath,
+  } = el.dataset;
+
+  const movie = {
+    id: Number(mvid),
+    genre_ids: genreIds.split(',').map(value => Number(value)),
+    title: titleString.split('_').join(' '),
+    original_title: originalTitleString.split('_').join(' '),
+    poster_path: posterPath,
+    release_date: releaseDate,
+  };
+
+  movies.push(movie);
+  save(key, movies);
+};
+
+export const removeMovie = (movieId, movies, key) => {
+  const index = movies.findIndex(({ id }) => id === movieId);
+  movies.splice(index, 1);
+  save(key, movies);
+};
+
+export const handleModalBtnClick = ({ key, id, el, listName }) => {
   const storedMovies = load(key) ?? [];
 
-  return storedMovies.map(({ id }) => id).includes(id);
+  if (storedMovies.length > 0) {
+    const isStoredMovie = checkMovieIsStored(storedMovies, id);
+
+    if (isStoredMovie) {
+      removeMovie(id, storedMovies, key);
+      el.textContent = `ADD TO ${listName}`;
+      el.classList.remove('modal__btn--added');
+    } else {
+      saveMovie(el, storedMovies, key);
+      el.textContent = `REMOVE FROM ${listName}`;
+      el.classList.add('modal__btn--added');
+    }
+  } else {
+    saveMovie(el, storedMovies, key);
+    el.textContent = `REMOVE FROM ${listName}`;
+    el.classList.add('modal__btn--added');
+  }
 };
