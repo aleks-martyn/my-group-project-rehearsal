fe38d41f5a4e38fcc0c4123e2c113b141d580d4c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-02-17 14:47:26 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,15 +2,28 @@ import * as basicLightbox from 'basiclightbox';
 import { moviesEl, bodyEl } from './ref-index';
 import getMovieById from './get-movie-by-id';
 import getTrailer from './get-trailer';
+import {
+  load,
+  checkMovieIsStored,
+  handleModalBtnClick,
+} from './local-storage-functions';
+import { changeBtnText } from './change-btn-text';
+import { WATCHED_MOVIES, QUEUE_MOVIES, WATCHED, QUEUE } from './constants';
 
 moviesEl.addEventListener('click', handleMovieClick);
 
 async function handleMovieClick(event) {
-  const movieId = event.target.dataset?.mvid;
+  const movieId = Number(event.target.dataset?.mvid);
 
   if (!movieId) return;
 
   const movie = await getMovieById(movieId);
+
+  const storedWatchedMovies = load(WATCHED_MOVIES) ?? [];
+  const isStoredWatchedMovie = checkMovieIsStored(storedWatchedMovies, movieId);
+
+  const storedQueueMovies = load(QUEUE_MOVIES) ?? [];
+  const isStoredQueueMovie = checkMovieIsStored(storedQueueMovies, movieId);
 
   const modalInstance = basicLightbox.create(movie, {
     onClose: () => {
@@ -28,10 +41,43 @@ async function handleMovieClick(event) {
   modalInstance.show(() => {
     const closeBtnEl = document.querySelector('.modal__close');
     const playBtnEl = document.querySelector('.modal__play');
+    const watchedBtnEl = document.querySelector('.modal__btn--watched');
+    const queueBtnEl = document.querySelector('.modal__btn--queue');
+
+    watchedBtnEl.textContent = changeBtnText(isStoredWatchedMovie, WATCHED);
+    queueBtnEl.textContent = changeBtnText(isStoredQueueMovie, QUEUE);
+
+    if (isStoredWatchedMovie) {
+      watchedBtnEl.classList.add('modal__btn--added');
+    }
+
+    if (isStoredQueueMovie) {
+      queueBtnEl.classList.add('modal__btn--added');
+    }
+
+    const watchedParams = {
+      key: WATCHED_MOVIES,
+      id: movieId,
+      el: watchedBtnEl,
+      listName: WATCHED,
+    };
+    
+    const queueParams = {
+      key: QUEUE_MOVIES,
+      id: movieId,
+      el: queueBtnEl,
+      listName: QUEUE,
+    };
 
     document.addEventListener('keydown', handleKeydownClick);
     closeBtnEl.addEventListener('click', handleCloseBtnClick);
     playBtnEl.addEventListener('click', handlePlayBtnClick);
+    watchedBtnEl.addEventListener('click', () =>
+      handleModalBtnClick(watchedParams)
+    );
+    queueBtnEl.addEventListener('click', () =>
+      handleModalBtnClick(queueParams)
+    );
     bodyEl.classList.add('no-scroll');
 
     async function handlePlayBtnClick(event) {
