bc33626583ad1d81d993952aac93eb63e19e5128
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-02-18 19:25:43 +0200

    Update get-stored-movies.js

--- src/js/get-stored-movies.js
+++ src/js/get-stored-movies.js
@@ -1,14 +1,24 @@
-import { moviesEl } from './ref-index';
+import { moviesEl, headerWatchedBtnEl, headerQueueBtnEl } from './ref-index';
+import getSelectedList from './get-selected-list';
 import renderGallery from './render-gallery';
 import { load } from './local-storage-functions';
-import { WATCHED_MOVIES } from './constants';
+import { WATCHED_MOVIES, QUEUE_MOVIES } from './constants';
 
 async function getStoredMovies() {
-  const movies = load(WATCHED_MOVIES) ?? [];
+  headerQueueBtnEl.classList.add('lib-btn--current');
+
+  const storedMovies = load(QUEUE_MOVIES) ?? [];
+
+  headerWatchedBtnEl.addEventListener('click', () =>
+    getSelectedList(headerQueueBtnEl, headerWatchedBtnEl, WATCHED_MOVIES)
+  );
+  headerQueueBtnEl.addEventListener('click', () =>
+    getSelectedList(headerWatchedBtnEl, headerQueueBtnEl, QUEUE_MOVIES)
+  );
 
   try {
-    const res = await renderGallery(movies);
-    return (moviesEl.innerHTML = res);
+    const markup = await renderGallery(storedMovies);
+    return (moviesEl.innerHTML = markup);
   } catch (error) {
     console.log(error.message);
   }
