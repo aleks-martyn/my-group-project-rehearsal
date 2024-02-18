17a2c1aae0136250ab6149b8feaefb034e3a457f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-02-18 19:22:29 +0200

    Create get-selected-list.js

---
+++ src/js/get-selected-list.js
@@ -0,0 +1,17 @@
+import { moviesEl } from "./ref-index";
+import { load } from "./local-storage-functions";
+import renderGallery from "./render-gallery";
+
+export default async function getSelectedList(firstEl, secondEl, key) {
+  firstEl.classList.remove('lib-btn--current');
+  secondEl.classList.add('lib-btn--current');
+
+  const storedMovies = load(key) ?? [];
+
+  try {
+    const markup = await renderGallery(storedMovies);
+    return (moviesEl.innerHTML = markup);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
