ecb9bc4c61f89b16bf64e074ca991e1d4cdb91cf
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-04 23:00:12 +0200

    Update ref-index.js

--- src/js/ref-index.js
+++ src/js/ref-index.js
@@ -7,6 +7,10 @@ const bodyEl = document.querySelector('body');
 const homeBtnEl = document.querySelector('.nav__link--home');
 const headerWatchedBtnEl = document.querySelector('.lib-btn--watched');
 const headerQueueBtnEl = document.querySelector('.lib-btn--queue');
+const labelEl = document.querySelector('.check__label');
+const checkBoxEl = document.querySelector('.check__input');
+const sunIconEl = document.querySelector('.check__icon-sun');
+const moonIconEl = document.querySelector('.check__icon-moon');
 
 export {
   searchFormEl,
@@ -18,4 +22,8 @@ export {
   headerWatchedBtnEl,
   headerQueueBtnEl,
   homeBtnEl,
+  labelEl,
+  checkBoxEl,
+  sunIconEl,
+  moonIconEl,
 };
