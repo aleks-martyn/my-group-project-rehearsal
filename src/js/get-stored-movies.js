02a493b99faaefa2813919b431b6c03d6336a28b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-12 21:38:49 +0200

    Update get-stored-movies.js

--- src/js/get-stored-movies.js
+++ src/js/get-stored-movies.js
@@ -3,11 +3,12 @@ import getSelectedList from './get-selected-list';
 import renderGallery from './render-gallery';
 import { load } from './local-storage-functions';
 import { WATCHED_MOVIES, QUEUE_MOVIES } from './constants';
+import createPagination from './create-pagination';
+import getCurrentPageMovies from './get-current-page-movies';
+import setScrollToUp from './set-scroll';
 
 async function getStoredMovies() {
   headerQueueBtnEl.classList.add('lib-btn--current');
-
-  const storedMovies = load(QUEUE_MOVIES) ?? [];
 
   headerWatchedBtnEl.addEventListener('click', () =>
     getSelectedList(headerQueueBtnEl, headerWatchedBtnEl, WATCHED_MOVIES)
@@ -16,11 +17,40 @@ async function getStoredMovies() {
     getSelectedList(headerWatchedBtnEl, headerQueueBtnEl, QUEUE_MOVIES)
   );
 
-  try {
-    const markup = await renderGallery(storedMovies);
-    return (moviesEl.innerHTML = markup);
-  } catch (error) {
-    console.log(error.message);
+  const storedMovies = load(QUEUE_MOVIES) ?? [];
+  const movieQuantity = storedMovies.length;
+  const moviesPerPage = 4;
+
+  const visiblePages = 3;
+
+  if (movieQuantity > 0) {
+    const visibleMovies = getCurrentPageMovies(storedMovies, moviesPerPage);
+
+    const pagination = createPagination(
+      movieQuantity,
+      moviesPerPage,
+      visiblePages
+    );
+
+    try {
+      pagination.on('afterMove', async event => {
+        setScrollToUp();
+        const currentPage = event.page;
+        const visibleMovies = getCurrentPageMovies(
+          storedMovies,
+          moviesPerPage,
+          currentPage
+        );
+
+        const markup = await renderGallery(visibleMovies);
+        return (moviesEl.innerHTML = markup);
+      });
+
+      const markup = await renderGallery(visibleMovies);
+      return (moviesEl.innerHTML = markup);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
 
