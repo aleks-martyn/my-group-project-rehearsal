29c58a30687a4d695df1c9b5bbb8209d91d778d4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-12 21:34:20 +0200

    Update get-selected-list.js

--- src/js/get-selected-list.js
+++ src/js/get-selected-list.js
@@ -1,16 +1,44 @@
 import { moviesEl } from './ref-index';
 import { load } from './local-storage-functions';
 import renderGallery from './render-gallery';
+import createPagination from './create-pagination';
+import getCurrentPageMovies from './get-current-page-movies';
+import setScrollToUp from './set-scroll';
 
 export default async function getSelectedList(firstEl, secondEl, key) {
   firstEl.classList.remove('lib-btn--current');
   secondEl.classList.add('lib-btn--current');
 
   const storedMovies = load(key) ?? [];
+  const movieQuantity = storedMovies.length;
+  const moviesPerPage = 4;
 
-  if (storedMovies.length > 0) {
+  const visiblePages = 3;
+
+  if (movieQuantity > 0) {
+    const visibleMovies = getCurrentPageMovies(storedMovies, moviesPerPage);
+
+    const pagination = createPagination(
+      movieQuantity,
+      moviesPerPage,
+      visiblePages
+    );
+
     try {
-      const markup = await renderGallery(storedMovies);
+      pagination.on('afterMove', async event => {
+        setScrollToUp();
+        const currentPage = event.page;
+        const visibleMovies = getCurrentPageMovies(
+          storedMovies,
+          moviesPerPage,
+          currentPage
+        );
+
+        const markup = await renderGallery(visibleMovies);
+        return (moviesEl.innerHTML = markup);
+      });
+
+      const markup = await renderGallery(visibleMovies);
       return (moviesEl.innerHTML = markup);
     } catch (error) {
       console.log(error.message);
