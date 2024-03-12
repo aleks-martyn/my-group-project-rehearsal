bf7f9f45046913422d3bfdffd85c95f8e83a2be4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2024-03-12 21:47:23 +0200

    Update handle-modal-btn-click.js

--- src/js/handle-modal-btn-click.js
+++ src/js/handle-modal-btn-click.js
@@ -1,6 +1,10 @@
 import { getUpdatedMovies } from './get-updated-movies';
 import renderGallery from './render-gallery';
 import { moviesEl } from './ref-index';
+import createPagination from './create-pagination';
+import getCurrentPageMovies from './get-current-page-movies';
+import setScrollToUp from './set-scroll';
+import getTargetPage from './get-target-page';
 
 export default async function handleModalBtnClick(hasClass, el, paramsObj) {
   if (hasClass === false) {
@@ -9,14 +13,49 @@ export default async function handleModalBtnClick(hasClass, el, paramsObj) {
     if (hasCurrentClass === false) {
       getUpdatedMovies(paramsObj);
     } else {
-      const movies = getUpdatedMovies(paramsObj);
+      const targetPage = getTargetPage(paramsObj);
+      const updatedMovies = getUpdatedMovies(paramsObj);
+      const movieQuantity = updatedMovies.length;
+      const moviesPerPage = 4;
 
-      try {
-        const markup = await renderGallery(movies);
+      const visiblePages = 3;
 
-        return (moviesEl.innerHTML = markup);
-      } catch (error) {
-        console.log(error.message);
+      if (movieQuantity > 0) {
+        const visibleMovies = getCurrentPageMovies(
+          updatedMovies,
+          moviesPerPage,
+          targetPage
+        );
+
+        const pagination = createPagination(
+          movieQuantity,
+          moviesPerPage,
+          visiblePages
+        );
+
+        if (targetPage) {
+          pagination.movePageTo(targetPage);
+        }
+
+        try {
+          pagination.on('afterMove', async event => {
+            setScrollToUp();
+            const currentPage = event.page;
+            const visibleMovies = getCurrentPageMovies(
+              updatedMovies,
+              moviesPerPage,
+              currentPage
+            );
+
+            const markup = await renderGallery(visibleMovies);
+            return (moviesEl.innerHTML = markup);
+          });
+
+          const markup = await renderGallery(visibleMovies);
+          return (moviesEl.innerHTML = markup);
+        } catch (error) {
+          console.log(error.message);
+        }
       }
     }
   } else {
